42e497ecbec2143229da48b2b2263a8c42438b10
ujjwalsainiT <ujjwalreactjsdeveloper@gmail.com>
2021-11-09 10:03:29 -0800

    ujjwal

---
+++ src/Common/Header/Header.jsx
@@ -0,0 +1,98 @@
+import React, { useState } from 'react';
+
+//material ui appbar
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+
+//css file
+import "./Header.css";
+
+//logo
+import logo from "./logo.png";
+
+const Header = (props) => {
+
+    /*local state */
+    const [Sidebar, setSidebar] = useState(false);
+
+    /*function to open a sidebar */
+    const setLoadingnewside = () => {
+        document.getElementById("mySidenav").style.width = "250px";
+        setSidebar(true)
+    };
+
+    /*function to close a sidebar */
+    const Closesidebar = () => {
+        document.getElementById("mySidenav").style.width = "0px";
+        setSidebar(false)
+    }
+    return (
+        <div className="topheader">
+            <AppBar position="fixed" className="MainHeader">
+                <Toolbar className="header_padding">
+                    <div>
+                        <img src={logo} alt="" style={{ height: "55px" }} />
+                    </div>
+                    <div className="header_grow" />
+                    <div className="header_links">
+                        <span className="header_link_color">Features</span>
+                        <span className="header_link_color">Pricing</span>
+                        <span className="header_link_color">Blog</span>
+                        <span className="header_link_color">Gallery</span>
+                        <span className="header_link_color">English</span>
+                        <span className="header_link_color">
+                            <button className="header_button">Login</button>
+                        </span>
+                        <span className="header_link_color">
+                            <button className="header_button">Sign Up</button>
+                        </span>
+                    </div>
+                    <div className="mobile_Burger_Menu">
+                        <span
+                            className="logout_Pointer_cursor mr-3 text-right mt-2"
+                            onClick={!Sidebar ? setLoadingnewside : Closesidebar}
+                        >
+                            <i class="fa fa-bars"></i>
+                        </span>
+
+                        <div id="mySidenav" className="sidenav">
+                            <div className="cross_icon_style">
+                                <i
+                                    class="fa fa-times cursor"
+                                    onClick={() => {
+                                        document.getElementById("mySidenav").style.width =
+                                            "0px";
+                                        setSidebar(false);
+                                    }}
+                                ></i>
+                            </div>
+                            <span className="logout_Pointer_cursor">
+                                Features
+                            </span>
+                            <span className="logout_Pointer_cursor">
+                                Pricing
+                            </span>
+                            <span className="logout_Pointer_cursor">
+                                Blog
+                            </span>
+                            <span className="logout_Pointer_cursor">
+                                Gallery
+                            </span>
+                            <span className="logout_Pointer_cursor">
+                                English
+                            </span>
+                            <span className="logout_Pointer_cursor">
+                                Login
+                            </span>
+                            <span className="logout_Pointer_cursor">
+                                SignUp
+                            </span>
+                        </div>
+                    </div>
+                </Toolbar>
+            </AppBar>
+        </div>
+    )
+}
+
+export default Header
---
+++ src/Component/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import HOC from "../../Common/HOC";
+import { Grid, Card, Button } from '@material-ui/core';
+import "./Home.css";
+
+function Home() {
+    return (
+        <>
+           
+        </>
+    )
+}
+
+export default HOC(Home)
